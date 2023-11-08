450b1f836fb28f641b43055fa4bb8911441da502
ye-yu <30603031+ye-yu@users.noreply.github.com>
2023-11-08 08:21:53 +0000

    Add HandlebarsConfig and default value

--- src/handlebars/shared/constants.ts
+++ src/handlebars/shared/constants.ts
@@ -1,1 +1,7 @@
+import { HandlebarsConfig } from '../types/handlebars-config.type';
+
 export const HANDLEBARS_CLASS_ID = 'HANDLEBARS.CLASS_ID';
+export const HANDLEBARS_CONFIG = 'HANDLEBARS.CONFIG';
+export const HANDLEBARS_DEFAULT_CONFIG: HandlebarsConfig = {
+  alwaysReload: false,
+};
---
+++ src/handlebars/types/handlebars-config.type.ts
@@ -0,0 +1,1 @@
+export type HandlebarsConfig = { alwaysReload: boolean };

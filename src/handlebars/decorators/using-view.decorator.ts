756d515079aa9f09c31c5a51b419e860b1417502
ye-yu <30603031+ye-yu@users.noreply.github.com>
2023-11-08 07:46:35 +0000

    Do @Render on get functions

--- src/handlebars/decorators/using-view.decorator.ts
+++ src/handlebars/decorators/using-view.decorator.ts
@@ -1,11 +1,15 @@
 import { randomUUID } from 'crypto';
 import { HANDLEBARS_CLASS_ID } from '../shared/constants';
 import { ViewConfiguration, ViewKeys } from '../shared/storage';
+import { Logger, Render, RequestMethod, UseInterceptors } from '@nestjs/common';
+import { PolyfillResponse } from '../interceptors/polyfill-response.interceptor';
+import { METHOD_METADATA } from '@nestjs/common/constants';
 
+const logger = new Logger('UsingView');
 export function UsingView(path: any): ClassDecorator {
-  return (target: any) => {
+  return (classFn: any) => {
     let objectKey = Object.getOwnPropertyDescriptor(
-      target,
+      classFn,
       HANDLEBARS_CLASS_ID,
     );
     if (!objectKey) {
@@ -14,10 +18,34 @@ export function UsingView(path: any): ClassDecorator {
       objectKey.writable = false;
       objectKey.enumerable = false;
       objectKey.value = randomUUID();
-      Object.defineProperty(target, HANDLEBARS_CLASS_ID, objectKey);
+      logger.debug(`Assigning ${classFn.name} to ID ${objectKey.value}`);
+      Object.defineProperty(classFn, HANDLEBARS_CLASS_ID, objectKey);
     }
-    ViewKeys.set(objectKey.value, target);
+    ViewKeys.set(objectKey.value, classFn);
     ViewConfiguration.set(objectKey.value, path);
-    return null;
+
+    const names = Object.getOwnPropertyNames(classFn.prototype);
+    for (const propertyName of names) {
+      const descriptor = Object.getOwnPropertyDescriptor(
+        classFn.prototype,
+        propertyName,
+      );
+      if (!descriptor) {
+        continue;
+      }
+      const requestMethod = Reflect.getMetadata(
+        METHOD_METADATA,
+        descriptor.value,
+      );
+      if (requestMethod !== RequestMethod.GET) {
+        continue;
+      }
+      const viewName = `${objectKey.value}.${propertyName}`;
+      logger.debug(
+        `Applying @Render('${classFn.name}.${propertyName}') to ${propertyName}`,
+      );
+      Render(viewName)(classFn, propertyName, descriptor);
+    }
+    return UseInterceptors(PolyfillResponse)(classFn);
   };
 }

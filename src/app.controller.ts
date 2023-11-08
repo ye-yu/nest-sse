c2380641945f98879ded14496082f664f086b484
ye-yu <30603031+ye-yu@users.noreply.github.com>
2023-11-08 07:49:44 +0000

    Add get hello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UsingView } from './handlebars/decorators/using-view.decorator';
 
 @Controller()
+@UsingView('./views')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return { message: this.appService.getHello() };
   }
 }
